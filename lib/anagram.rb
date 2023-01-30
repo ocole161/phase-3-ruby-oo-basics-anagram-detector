84b9b399781dc13e121f344ab91628d1f9573b30
Owen Cole <ocole161@gmail.com>
2023-01-30 16:51:30 -0700

    commit tree 2c8342e9f603a35a54e7c79d9d0937698a847ede

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        matches = []
+        string.map do |word|
+            if @word.chars.sort == word.chars.sort
+                matches.push(word)
+            end
+        end
+        matches
+    end
+end
